f367a142d621f337c3f8a4d007af4ca68d54573e
Liu He <125852364@qq.com>
2021-06-16 15:46:50 +0100

    add localstorage

--- pages/_app.js
+++ pages/_app.js
@@ -7,21 +7,32 @@ import store from "../components/redux/store";
 import Layout from "../components/Layout";
 import { hydrateBasket } from "../components/redux/basketSlice";
 
+// try & catch in case local storage is disabled
+try {
+  store.subscribe(() => {
+    const basket = store.getState().basket;
+    // save basket to local storage
+    localStorage.setItem("name-brand-basket", JSON.stringify(basket));
+  });
+} catch (err) {
+  console.error(err);
+}
+
 export default function MyApp({ Component, pageProps }) {
-  // useEffect(() => {
-  //   // try & catch in case local storage is disabled
-  //   // local storage is undefined initially in next.js due to SSR
-  //   try {
-  //     if (typeof localStorage !== "undefined") {
-  //       const storedBasket = localStorage.getItem("name-brand-basket");
-  //       storedBasket
-  //         ? store.dispatch(hydrateBasket(JSON.parse(storedBasket)))
-  //         : store.dispatch(hydrateBasket({ item: [] }));
-  //     }
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }, []);
+  // fetch items from localstorage and update the basket if items exist
+  useEffect(() => {
+    // local storage is undefined initially in next.js due to SSR
+    try {
+      if (typeof localStorage !== "undefined") {
+        const storedBasket = localStorage.getItem("name-brand-basket");
+        storedBasket
+          ? store.dispatch(hydrateBasket(JSON.parse(storedBasket)))
+          : store.dispatch(hydrateBasket({ item: [] }));
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
 
   return (
     <AuthProvider session={pageProps.session}>
