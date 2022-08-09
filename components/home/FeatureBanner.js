1d0b5a1960c162ebcdc93b0d8becc559d6ddf9fc
Liu He <lhe19@yahoo.co.uk>
2022-08-09 20:35:57 +0100

    updated image load priority on feature banner

--- components/home/FeatureBanner.js
+++ components/home/FeatureBanner.js
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import CurrencyFormat from "../ui/CurrencyFormat";
-import ButtonDark from "../ui/ButtonDark";
+import CurrencyFormat from '../ui/CurrencyFormat';
+import ButtonDark from '../ui/ButtonDark';
 
 export default function FeatureBanner({ products }) {
-  const [selected, setSelected] = useState("Headphones");
+  const [selected, setSelected] = useState('Headphones');
 
   return (
-    <div className="overflow-hidden max-w-3xl my-0 mx-auto p-5 bg-white h-250 sm:h-275 md:h-250 lg:h-200">
-      <h1 className="text-xl mb-5 font-semibold text-center lg:text-left lg:m-5">
+    <div className='overflow-hidden max-w-3xl my-0 mx-auto p-5 bg-white h-250 sm:h-275 md:h-250 lg:h-200'>
+      <h1 className='text-xl mb-5 font-semibold text-center lg:text-left lg:m-5'>
         Top Of The Range
       </h1>
-      <div className="relative text-center pt-5 lg:text-left">
+      <div className='relative text-center pt-5 lg:text-left'>
         {products.map(({ type }) => (
           <span
             key={type}
@@ -31,40 +31,41 @@ export default function FeatureBanner({ products }) {
         {products.map(({ name, id, price, description, image, type }) => (
           <div
             key={id}
-            className="absolute top-1/2 lg:3/4 left-0 right-0 mx-auto"
+            className='absolute top-1/2 lg:3/4 left-0 right-0 mx-auto'
           >
             {selected === type && (
               <>
                 <motion.div
                   initial={{ opacity: 0, y: 150 }}
                   animate={{ opacity: 1, y: 0 }}
-                  transition={{ delay: 0.4, duration: 1, type: "tween" }}
-                  className="pointer-events-none w-4/5 md:w-1/2 lg:w-100 xl:w-128 mx-auto lg:mx-0 lg:absolute lg:-top-24 lg:left-32 2xl:left-64 2xl:-top-40"
+                  transition={{ delay: 0.4, duration: 1, type: 'tween' }}
+                  className='pointer-events-none w-4/5 md:w-1/2 lg:w-100 xl:w-128 mx-auto lg:mx-0 lg:absolute lg:-top-24 lg:left-32 2xl:left-64 2xl:-top-40'
                 >
                   <Image
+                    priority
                     width={600}
                     height={600}
-                    objectFit="contain"
+                    objectFit='contain'
                     src={image}
                   />
                 </motion.div>
                 <motion.div
                   initial={{ opacity: 0, y: 150 }}
                   animate={{ opacity: 1, y: 0 }}
-                  transition={{ delay: 0.4, duration: 1, type: "tween" }}
-                  className="lg:absolute lg:w-2/5 lg:-right-0 lg:top-36"
+                  transition={{ delay: 0.4, duration: 1, type: 'tween' }}
+                  className='lg:absolute lg:w-2/5 lg:-right-0 lg:top-36'
                 >
-                  <h2 className="text-3xl font-semibold mt-5">{name}</h2>
-                  <div className="mt-5 max-w-xs mx-auto lg:mx-0">
+                  <h2 className='text-3xl font-semibold mt-5'>{name}</h2>
+                  <div className='mt-5 max-w-xs mx-auto lg:mx-0'>
                     {description}
                   </div>
-                  <div className="text-2xl mt-10">
+                  <div className='text-2xl mt-10'>
                     <CurrencyFormat value={price} />
                   </div>
-                  <div className="mt-5">
+                  <div className='mt-5'>
                     <Link href={`/products/${id}`}>
                       <a>
-                        <ButtonDark text={"Buy now"} />
+                        <ButtonDark text={'Buy now'} />
                       </a>
                     </Link>
                   </div>
